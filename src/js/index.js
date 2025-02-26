d907ef335fc5666096f542204d1f21c103f500a1
Temistakes <temistakes@yandex.ru>
2025-02-26 18:23:26 +0300

    First commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,5 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+// Libraries
+
+import fslightbox from "fslightbox";
+import "./modules/sliders.js";
+import $ from "jquery";
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const config = {
 	entry: {
 		index: './src/js/index.js'
 		// contacts: './src/js/contacts.js',
-		// about: './src/js/about.js',
 	},
 	output: {
 		filename: '[name].bundle.js',
